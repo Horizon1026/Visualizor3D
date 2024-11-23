41f1cda0332172a3e75fd1d898ecfda879d79744
Horizon1026 <lijiapeng_atlantis@163.com>
2024-11-23 21:16:43 +0800

    update code

--- src/visualizor_3d.h
+++ src/visualizor_3d.h
@@ -1,7 +1,7 @@
 #ifndef _SLAM_UTILITY_3D_VISUALIZOR_H_
 #define _SLAM_UTILITY_3D_VISUALIZOR_H_
 
-#include "datatype_basic.h"
+#include "basic_type.h"
 #include "datatype_image.h"
 #include "slam_log_reporter.h"
 
